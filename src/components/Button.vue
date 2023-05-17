37f1692929970b25a792ffc52fb2bbf8e8875a0b
Josneth Moreno <josnethmoreno@gmail.com>
2023-05-17 13:48:06 -0400

    styles

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+export default {
+  name: "Button",
+}
+</script>
+
+<template>
+  <form action="">
+    <input type="text">
+    <button data-submit="submit">Notificame!</button>
+  </form>
+</template>
+
+<style scoped>
+button {
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import Button from './Button.vue';
+
+export default {
+  name: "Hero",
+  components: {
+    Button
+  }
+}
+</script>
+
+<template>
+  <div class="hero">
+    <h1>El futuro de la IA es en <br> nuestro idioma</h1>
+    <p class="lead"><b>Suscribete</b> y no te pierdas todo lo que se viene del lanzamiento de <br> nuestra versión beta. Muy pronto!</p>
+    <Button></Button>
+  </div>
+</template>
+
+<style scoped>
+  .hero {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    gap: 1.6rem;
+  }
+</style>
